0bce4b11d601c52caa7b89cd8068d08f3375ef7b
Murat Süzen <muratsuzen@gmail.com>
2019-08-28 23:41:59 +0300

    product service eklendi

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { AlertifyService } from './services/alertify.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [AlertifyService],
   bootstrap: [AppComponent]
--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { AlertifyService } from '../services/alertify.service';
+import { ProductService } from '../services/product.service';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private alertifyService:AlertifyService) { }
+  constructor(
+    private alertifyService: AlertifyService,
+    private productService: ProductService
+  ) { }
   title = "Ürün Listesi"
   filterText = ""
-  products : Product[] = [
-    {id:1, name:"Laptop", price:2500, categoryId:1, description : "Asus ZenBook",imageUrl:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
-    {id:2, name:"Mouse", price:25, categoryId:2, description : "A4 Tech",imageUrl:"https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"},
-  ]
+  products: Product[];
 
   ngOnInit() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data
+    });
   }
 
-  addToCard(product){
+  addToCard(product) {
     this.alertifyService.success(product.name + " added")
   }
 }
