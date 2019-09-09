c545d2eb9ccaca284f156a4f243ff53eee89b62e
Murat Süzen <muratsuzen@gmail.com>
2019-09-09 23:30:07 +0300

    SehirRehberi.SPA güncelleme

--- Project3/SehirRehberi-SPA/src/app/app.module.ts
+++ Project3/SehirRehberi-SPA/src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import {RouterModule, Router} from "@angular/router";
+import {appRoutes} from "./routes";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent,
+      CityComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
