99ec8e1a4fba9c468c11885f87faef9dd9d02367
Murat Süzen <muratsuzen@gmail.com>
2019-09-10 23:26:33 +0300

    SehirRehberi.SPA Şehir ekleme

--- Project3/SehirRehberi-SPA/src/app/app.module.ts
+++ Project3/SehirRehberi-SPA/src/app/app.module.ts
@@ -1,31 +1,38 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import {RouterModule, Router} from "@angular/router";
-import {appRoutes} from "./routes";
+import { RouterModule, Router } from "@angular/router";
+import { appRoutes } from "./routes";
+import { NgxGalleryModule } from "ngx-gallery";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
-import { NavComponent } from './nav/nav.component';
-import { CityComponent } from './city/city.component';
+import { NavComponent } from "./nav/nav.component";
+import { CityComponent } from "./city/city.component";
+import { CityDetailComponent } from "./city/city-detail/city-detail.component";
+import { CityAddComponent } from "./city/city-add/city-add.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent,
-      NavComponent,
-      CityComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      HttpClientModule,
-      RouterModule.forRoot(appRoutes)
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent,
+    CityComponent,
+    CityDetailComponent,
+    CityAddComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- Project3/SehirRehberi-SPA/src/app/services/city.service.ts
+++ Project3/SehirRehberi-SPA/src/app/services/city.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { City } from '../models/city';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { City } from "../models/city";
+import { Photo } from "../models/Photo";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CityService {
+  constructor(private httpClient: HttpClient) {}
+  path = "http://localhost:50017/api/";
 
-constructor(private httpClient:HttpClient) { }
-path = "http://localhost:50017/api/";
+  getCities(): Observable<City[]> {
+    return this.httpClient.get<City[]>(this.path + "cities");
+  }
 
-getCities():Observable<City[]>{
-  return this.httpClient.get<City[]>(this.path+"cities");
-}
+  getCityById(cityId): Observable<City> {
+    return this.httpClient.get<City>(this.path + "cities/detail/?id=" + cityId);
+  }
 
+  getPhotosByCity(cityId): Observable<Photo[]> {
+    return this.httpClient.get<Photo[]>(
+      this.path + "cities/photos/?cityId=" + cityId
+    );
+  }
 
+  add(city){
+    this.httpClient.post(this.path+"cities/add",city).subscribe();
+  }
 }
